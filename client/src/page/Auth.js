488f21c9cff666dc3e0eccf6eedcef9f3164fc86
Mikita <vostrymikita@gmail.com>
2022-08-13 23:32:17 +0200

    auth page added

--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Button, Container, Form, Row } from 'react-bootstrap'
 import Card from 'react-bootstrap/Card';
-import { NavLink } from 'react-router-dom';
-import { REGISTRATION_ROUTE } from '../utils/consts'
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
 const Auth = () => {
+
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
     return (
         <Container
             className='d-flex justify-content-center align-items-center'
             style={{ heigth: window.innerHeight - 54 }}
         >
             <Card style={{ width: 600 }} className='p-5'>
-                <h2 className='s-auto'>Log in</h2>
+                <h2 className='s-auto'>{isLogin ? 'Log in' : 'Registration'}</h2>
                 <Form className='d-flex flex-column'>
                     <Form.Control
                         className='mt-3'
@@ -21,12 +24,23 @@ const Auth = () => {
                         placeholder='enter password'
                     />
                     <Row className='d-flex justify-content-between mt-3 pl-3 pr-3'>
-                        <div>
-                            Have no account?<NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
-                        </div>
+                        {isLogin ?
+                            <div>
+                                Have no account?<NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Have an account?<NavLink to={LOGIN_ROUTE}>Log in</NavLink>
+                            </div>
+                        }
                         <Button
                             variant='outline-success'>
-                            Enter
+
+                            {isLogin ?
+                                'Enter'
+                                :
+                                'Register'
+                            }
                         </Button>
                     </Row>
 
